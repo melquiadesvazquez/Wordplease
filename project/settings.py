6ea68499bb8a2b70bd0208a616c2689644723f08
Melquiades Vazquez <melquiades.vazquez@gmail.com>
2018-11-23 19:54:58 +0100

    configuring heroku

--- project/settings.py
+++ project/settings.py
@@ -60,6 +60,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
 ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
